e412124a3cf0d770365bcef5c716b425e83c84a8
Robinho <robinhodemorais@gmail.com>
2016-07-07 09:16:07 -0300

    Relacionando

--- app/Product.php
+++ app/Product.php
@@ -26,4 +26,9 @@ class Product extends Model
     {
         return $this->belongsTo('CodeCommerce\Category');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('CodeCommerce\Tag');
+    }
 }
